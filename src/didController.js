017d36cfb28651aae8d60a465305a2f230e4e42b
Chris Were <chris@mozzler.com.au>
2020-02-29 13:12:57 +1030

    Remove duplicated load() action.

--- src/didController.js
+++ src/didController.js
@@ -59,32 +59,6 @@ class DidController {
         }
     }
 
-    async load(req, res) {
-        let did = req.query.did;
-
-        try {
-            let doc = await DbManager.loadDoc(did);
-
-            return res.status(200).send({
-                status: "success",
-                data: {
-                    document: doc.doc
-                }
-            });
-        } catch (err) {
-            if (err.reason == "missing") {
-                return res.status(400).send({
-                    status: "fail",
-                    data: {
-                        did: "Invalid DID or not found"
-                    }
-                });
-            }
-
-            throw err;
-        }
-    }
-
     async commit(req, res) {
         let document = req.body.params.document;
         let publicDid = req.body.params.did;
